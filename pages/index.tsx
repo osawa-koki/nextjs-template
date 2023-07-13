b710fa2cbea6180ddad9053013b6e871703fb75c
osawa-koki <github@osawa.dev>
2023-07-14 00:07:33 +0900

    スタイルの調整。

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,13 @@
 import React from 'react'
-import Link from 'next/link'
 import Layout from '../components/Layout'
-import pages from '../pages'
 import setting from '../setting'
 
 export default function Home (): JSX.Element {
   return (
-    <Layout menu={false} footer={false}>
-      <div id="Index">
+    <Layout>
+      <div id='Index' className='d-flex flex-column align-items-center'>
         <h1>{setting.title}</h1>
-        <img id="Logo" src={`${setting.basePath}/tako.png`} alt="Logo" />
-        <div id="IndexLink">
-          {pages.map((page, index: number) => {
-            return (
-              <Link key={index} href={page.path}>
-                {page.name}
-              </Link>
-            )
-          })}
-        </div>
+        <img id='Logo' className='mt-3' style={{ width: '100%', maxWidth: '100px' }} src={`${setting.basePath}/tako.png`} alt='Logo' />
       </div>
     </Layout>
   )
