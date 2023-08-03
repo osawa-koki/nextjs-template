df627937798081c494b4a64f9cf308d889a8c8bc
osawa-koki <koki@osawa.dev>
2023-08-04 00:11:18 +0900

    ESLintに関するNext.jsプラグインを導入する。

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ export default function Home (): JSX.Element {
     <Layout>
       <div id='Index' className='d-flex flex-column align-items-center'>
         <h1>{setting.title}</h1>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img id='Logo' className='mt-3' style={{ width: '100%', maxWidth: '100px' }} src={`${setting.basePath}/tako.png`} alt='Logo' />
       </div>
     </Layout>
